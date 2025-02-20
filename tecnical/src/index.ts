6b78a7dcf54798c1f5dc9205f90d808adc82fefb
denisetiya <denisetiya009@gmail.com>
2025-02-20 17:28:10 +0700

    update notfound handler

--- tecnical/src/index.ts
+++ tecnical/src/index.ts
@@ -9,10 +9,15 @@ const app: Application = express();
 app.use(express.json());
 app.use(router);
 
+app.use('*', (req, res) => {
+    res.status(404).send({
+        error: 'Not Found',
+        message: `Route ${req.originalUrl} not found`
+    });
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
 })
-
 
 
